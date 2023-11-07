1c8c2280b898e4c868b91b0993783f3e41520496
Selin Yigit <sln.ygt.41@gmail.com>
2023-11-07 22:48:25 +0300

    BreadCrumbProvider Class oluşturuldu.

---
+++ lib/breadCrumbProvider.dart
@@ -0,0 +1,25 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:testing_provider_project/breadCrumb.dart';
+
+class BreadCrumbProvider extends ChangeNotifier {
+  //BreadCrumb'ların tutulduğu liste.
+  final List<BreadCrumb> _items = [];
+
+  //BreadCrumb'ların tutulduğu READONLY liste.
+  UnmodifiableListView<BreadCrumb> get item => UnmodifiableListView(_items);
+
+  void Add(BreadCrumb breadCrumb) {
+    for (final item in _items) {
+      item.Activate();
+    }
+    _items.add(breadCrumb);
+    notifyListeners();
+  }
+
+  void Reset() {
+    _items.clear();
+    notifyListeners();
+  }
+}
